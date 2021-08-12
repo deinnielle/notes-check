39431ef87b4d8c48aec4c30ec895cabb2ec93c38
Mark Dela Cruz <mark.a.dela.cruz@ericsson.com>
2021-08-12 17:56:56 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const note = document.createElement("p");
+note.innerHTML = "bO";
+note.style.zIndex = 2;
+note.style.position = "fixed";
+note.style.left = "30%";
+note.style.top = "38px";
+document.body.appendChild(note);
